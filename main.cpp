4d1b884178fd96e4ae3d59587b2c8d76f92ff6a4
centos-borange <1027695107@qq.com>
2021-10-29 15:41:55 +0800

    创建了main.cpp,写了初始代码

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void sort(int arr[],int size)
+{
+
+}
+
+int main(){
+  int arr[] = {12,23,34,45,23};
+  int size = sizeof(arr)/sizeof(arr[0]);
+  sort(arr,size);
+  return 0;
+}
